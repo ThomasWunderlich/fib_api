767b9c54443c36363dd9b3e42570f771c60182cf
Thomas Wunderlich <twunde@gmail.com>
2024-04-18 23:23:13 -0400

    Add error type check

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ def read_root():
 
 @app.get("/fib/{num}")
 def fib_seq_check(num: int):
-    if is_fibbonaci(num):
-        message = calc_next_fib(num)
-    else:
-        message = "not a Fibonacci number."
-    return message
+    # FastAPI probably has a nice way to typecheck params but this will do for now
+    if not isinstance(num, int):
+        return "Error: num must be an integer."
 
-# Apparently, the fibbonaci sequence has a nice mathematical property to determine whether a number is part of the
+    if is_fibbonaci(num):
+        return calc_next_fib(num)
+    else:
+        return "not a Fibonacci number."
+
+# Apparently, the Fibonacci sequence has a nice mathematical property to determine whether a number is part of the
 # sequence.
-# R is a Fibinacci number only if at least one of (5 * R * R + 4) or (5 * R * R - 4) are perfect squares
+# R is a Fibonacci number only if at least one of (5 * R * R + 4) or (5 * R * R - 4) are perfect squares
 
 def is_perfect_square(num: int) -> bool:
     sqrt = int(m.sqrt(num))
