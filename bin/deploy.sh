bf523a1ba2a0f4574dba0e64ef597d7e1ae6a353
Thomas Wunderlich <twunde@gmail.com>
2024-04-22 11:27:39 -0400

    Commit deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+CONTAINER_NAME="${CONTAINER_NAME:-twunde764/fib:dev}"
+EC2_INSTANCE="${EC2_INSTANCE:-54.224.50.156}"
+
+# build and push the container
+docker build -t "$CONTAINER_NAME" .
+docker push "$CONTAINER_NAME"
+# reboot service. This will automatically pull the latest dev tag of the container.
+# If you want to use a different tag, you will need to update the user data script in the automation repo
+ssh ec2-user@$EC2_INSTANCE "sudo systemctl restart docker.fib"
+
+
